6a81e7e4a9d5d0d262d11b9ccd7161b1c2e33187
Khanh Nguyen <khanhtrantuc2004@gmail.com>
2024-08-20 22:03:30 +0700

    Update

--- Buoi1/script.js
+++ Buoi1/script.js
@@ -1,5 +1,17 @@
-var  button =document.querySelector("button");
-button.onclick= function () {
-    alert("Hello World");
+// var  button =document.querySelector("button");
+// button.onclick= function () {
+//     alert("Hello World");
 
-}
+// }
+
+var a =10;
+
+var b =20;
+
+var c =a+b;
+
+console.log(c);
+
+
+// camel case
+var hoTen ="Dang Phuong Nam"; 
