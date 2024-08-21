50b8688b6b02752e5ed447e560d039bd5e498bce
Khanh Nguyen <khanhtrantuc2004@gmail.com>
2024-08-21 14:34:28 +0700

    Update morning

---
+++ Buoi1/script2.js
@@ -0,0 +1,10 @@
+//05 . 1 số hàm built-in.
+
+// alert ("Xin chao Cac ban");
+
+//  var result =confirm ("Ban da tren 18 tuoi");
+
+//  console.log(result );
+
+var result =prompt("Nhâp n");
+console.log(result);
