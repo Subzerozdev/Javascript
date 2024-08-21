b7eff29caaa0493f8716c56bfb2c7d8c1495db86
Khanh Nguyen <khanhtrantuc2004@gmail.com>
2024-08-21 15:31:08 +0700

    7.3 lastIndexOf

--- Buoi1/script2.js
+++ Buoi1/script2.js
@@ -6,5 +6,66 @@
 
 //  console.log(result );
 
-var result =prompt("Nhâp n");
-console.log(result);
+// var result =prompt("Nhập n");
+// console.log(result);
+
+
+//Hàm set time out: 
+
+// setTimeout(function (){
+// console.log("A");
+// }, 1000);
+// hàm này sau 1 giấy nó sẽ in ra cái dòng này.
+
+
+// Hàm setInterval() :chạy lặp lại sau một khoảng thời gian.
+
+
+//  var b =setInterval(function (){
+//     console.log("B");
+//     }, 1000);
+
+
+//     setTimeout(function (){
+//         clearInterval(b)
+//     },5000);
+
+
+    // sau 5 giây nó gọi hàm setTimeOut == > chạy vào hàm clear Interval rồi dừng lại.
+
+//06. Type Of: cho phép trả về kiểu của một biến:
+
+// var  a =10;
+// var b ="Nội dung...";
+// var c;
+// var d =null;
+// var e =true;
+// console.log(typeof a); // //number
+// console.log(typeof b); // string
+// console.log(typeof (a+b));  //string
+// console.log(typeof c); //undefined
+// console.log(typeof d); // object
+// console.log(typeof e); //boolean
+// console.log(typeof a=="number"); // true
+
+// 07. Làm việc với String:
+
+
+// 7.1 hàm length để đếm số kí tự: 
+// var fullName ="nguyen Gia Khanh";
+// console.log(fullName.length);
+ 
+
+//7.2 indexOf(): tìm vị trí đầu tiên của một chuỗi nằm trong một chuỗi, không có thì trả về -1, có phân biệt hoa thường
+// var myString ="xin chào !, tôi tên là NGK, năm nay tôi 20 tuổi";
+// console.log(myString.indexOf("tôi"));
+// console.log(myString.indexOf("tôi",13)); // tính từ index =13 tới vị trí có từ tôi
+
+
+//7.3 lastIndexOf(): tìm vị trí cuối cùng của một chuỗi nằm trong một chuỗi, không có trả về -1, có phân biệt hoa thường.
+// cú pháp: String.lastIndexOf(searchvalue,start);
+// lưu ý**********: start: vị trí bắt đầu tìm kiếm ( tính từ trái qua phải và sẽ tìm kiếm ngược lại )
+// var myString ="xin chào !, tôi tên là NGK, năm nay tôi 20 tuổi";
+// console.log(myString.lastIndexOf("tôi"));
+// console.log(myString.lastIndexOf("tôi",100)); 
+
